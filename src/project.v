56648db3cb3faa03e42a724045b9363c00b38cd8
Roni Kant <35043400+Penguronik@users.noreply.github.com>
2024-10-07 17:44:37 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,6 +16,10 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  
+    
+  b_register register(.clk(clk),.n_load(ui_in [0]),.bus(ui_in),.value(uo_out));
+    
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
