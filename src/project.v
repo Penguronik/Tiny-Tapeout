18588252066645cc5b5b611458de331353250606
Jeremy-Kam <144426103+Jeremy-Kam@users.noreply.github.com>
2024-10-28 00:47:14 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,27 +16,27 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // // Register Test Start
-  // register b_register(.clk(clk),.n_load(uio_in [0]),.bus(ui_in),.value(uo_out));
-    
-  // // All output pins must be assigned. If not used, assign to 0.
-  // // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  // assign uio_out = 0;
-  // assign uio_oe  = 0;
-
-  // // List all unused inputs to prevent warnings
-  // wire _unused = &{ena, rst_n, 1'b0};
-  // // Register Test End
-
-  // Input and MAR Register Start
-    input_mar_register im_register( .clk(clk), .n_load_data(uio_in [0]), .n_load_addr(uio_in [1]), .bus(ui_in), .data(uo_out), .addr(uio_out[3:0]) );
+  // Register Test Start
+  register b_register(.clk(clk),.n_load(uio_in [0]),.bus(ui_in),.value(uo_out));
     
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, rst_n, 1'b0};
-  // Input and MAR Register End
+  // Register Test End
+
+  // // Input and MAR Register Start
+  //   input_mar_register im_register( .clk(clk), .n_load_data(uio_in [0]), .n_load_addr(uio_in [1]), .bus(ui_in), .data(uo_out), .addr(uio_out[3:0]) );
+    
+  // // All output pins must be assigned. If not used, assign to 0.
+  // // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_oe  = 0;
+
+  // // List all unused inputs to prevent warnings
+  // wire _unused = &{ena, rst_n, 1'b0};
+  // // Input and MAR Register End
 
 endmodule
