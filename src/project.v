46f575c6a457afd38a970f711a3d8bcdc1aa441e
Roni Kant <35043400+Penguronik@users.noreply.github.com>
2024-10-28 15:15:41 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_example (
 );
 
   // Register Test Start
-  register b_register(.clk(clk),.n_load(uio_in [0]),.bus(ui_in),.value(uo_out));
+  // register b_register(.clk(clk),.n_load(uio_in [0]),.bus(ui_in),.value(uo_out));
     
   // // All output pins must be assigned. If not used, assign to 0.
   // // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
@@ -32,7 +32,6 @@ module tt_um_example (
     input_mar_register im_register( .clk(clk), .n_load_data(uio_in [0]), .n_load_addr(uio_in [1]), .bus(ui_in), .data(uo_out), .addr(uio_out[3:0]) );
     
   // All output pins must be assigned. If not used, assign to 0.
-  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
