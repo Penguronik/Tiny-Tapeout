a4b6aff6bdb2792829e7dd905078f438e0cb7223
Jeremy-Kam <144426103+Jeremy-Kam@users.noreply.github.com>
2024-10-28 16:59:45 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -52,6 +52,7 @@ module tt_um_example (
     
     // All output pins must be assigned. If not used, assign to 0.
     assign uio_oe  = 0;
+    assign uio_out[7:4] = 0;
     
     // List all unused inputs to prevent warnings
     wire _unused = &{ena, rst_n, 1'b0};
